ea43d4b297e6019a558fe210b841450e69ba79b1
unknown <kithminavihanga2001@gmail.com>
2025-03-05 00:51:14 +0530

    style file linked for sign.php

--- pat/p_sign.php
+++ pat/p_sign.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patient Signup</title>
-    <link rel="stylesheet" href="patient_signup.css">
+    <link rel="stylesheet" href="p_sign.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 <body>
