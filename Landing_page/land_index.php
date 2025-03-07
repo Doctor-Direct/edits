c9183e33f54c31c5912eb920c99069a113c1ff7d
unknown <kithminavihanga2001@gmail.com>
2025-03-07 11:49:46 +0530

    Add Appoinment html and css files

--- Landing_page/land_index.php
+++ Landing_page/land_index.php
@@ -35,7 +35,7 @@
         <ul class="nav-links">
             <li><a href="land_index.php">Home</a></li>
             <li><a href="land_log_index.html">Login</a></li>
-            <li><a href="#">Hospital</a></li>
+            <li><a href="#">Appoinment</a></li>
             <li><a href="#">About Us</a></li>
         </ul>
     </nav>
